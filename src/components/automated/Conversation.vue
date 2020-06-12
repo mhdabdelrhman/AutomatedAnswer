b5f45d7d11fba840aeae1acff13d6b4fbffa894b
yazan <m.haffar89@gmail.com>
2020-06-12 14:54:30 +0300

    add methods to save to localStorage and get from it and timer to save data evert 2 seconds

--- src/components/automated/Conversation.vue
+++ src/components/automated/Conversation.vue
@@ -1,32 +1,47 @@
 <template>
-    <div class="conversation-container">
-        <chain-node v-model="chain"></chain-node>
-        <div>
-            <span @click="handelSave">Save</span>
-        </div>
+  <div class="conversation-container">
+    <chain-node v-model="chain"></chain-node>
+    <div>
+      <span @click="handelSave">Save</span>
     </div>
+  </div>
 </template>
 
 <script>
-    import tree from './models'
-    import ChainNode from './ChainNode'
-    export default {
-        name: 'conversation',
-        components: {
-            'chain-node': ChainNode,
-        },
-        data() {
-            return {
-                chain: tree,
-            }
-        },
-        methods: {
-            handelSave() {
-                console.log("chain:",JSON.stringify(this.chain));
-            }
-        }
-    }
+import tree from "./models";
+import ChainNode from "./ChainNode";
+export default {
+  name: "conversation",
+  components: {
+    "chain-node": ChainNode,
+  },
+  data() {
+    return {
+      chain: tree,
+    };
+  },
+  methods: {
+    handelSave() {
+      localStorage.setItem("chain", JSON.stringify(this.chain));
+      console.log("chain:", JSON.stringify(this.chain));
+    },
+    hanedelLoadChain() {
+      var newChain = JSON.stringify(this.chain);
+      newChain = localStorage.getItem("chain");
+      console.log(newChain);
+    },
+    timer() {
+      setInterval(() => {
+        this.handelSave();
+      }, 2000);
+    },
+  },
+  async created() {
+    await this.handelSave();
+    console.log("this go second");
+    await this.hanedelLoadChain();
+    this.timer();
+  },
+};
 </script>
-<style>
-
-</style>
+<style></style>
