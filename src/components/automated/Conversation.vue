b4e769f688fd56edff82c0ffe9ffce3dc92c5071
yazan <m.haffar89@gmail.com>
2020-06-12 19:34:38 +0300

    edit the code

--- src/components/automated/Conversation.vue
+++ src/components/automated/Conversation.vue
@@ -2,7 +2,7 @@
     <div class="conversation-container">
         <chain-node v-model="chain"></chain-node>
         <div>
-            <span @click="handelSave">Save</span>
+            <span @click="saveTreeToLocalStorage">Save</span>
         </div>
     </div>
 </template>
@@ -21,12 +21,26 @@
             }
         },
         methods: {
-            handelSave() {
+            saveTreeToLocalStorage() {
                 console.log("chain:",JSON.stringify(this.chain));
-            }
-        }
+                localStorage.setItem("chain", JSON.stringify(this.chain));
+            },
+            loadTreeFromLocalStorage(){
+                var loadedChain = JSON.parse(localStorage.getItem("chain"));
+                return loadedChain;
+            },
+            timer() {
+                setInterval(() => {
+                    this.saveTreeToLocalStorage();
+                }, 2000);
+    }
+        },
+        created() {
+            // i get error for this 
+            // this.chain = this.loadTreeFromLocalStorage()
+            this.timer()
+            //now this return chain every 2 sec and i test to get chain and it is updated in every 2 sec
+        },
     }
 </script>
-<style>
-
-</style>
+<style></style>
