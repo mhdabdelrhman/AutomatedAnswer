6389df7d1ad804bfb345f6ef6115c2d7fa0576e3
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2020-06-14 00:58:13 +0300

    (#12) add search service

--- src/components/automated/models.js
+++ src/components/automated/models.js
@@ -27,3 +27,49 @@ export class Dialog {
 }
 
 export var tree = new Chain();
+
+
+export class SearchService {
+	constructor(chain, searchText) {
+		this.searchText = searchText;
+		this.chain = chain;
+		this.results = [];
+		this.search();
+	};
+
+	search() {
+		this.results = [];
+		if (!this.chain || !this.searchText)
+			return;
+
+		if (this.isTextMatchSearch(this.chain.id) || this.isTextMatchSearch(this.chain.text))
+			this.results.push([]);
+
+		for (let i = 0; i < this.chain.replies.length; i++) {
+			this.searchRecursive([{ replies: this.chain.replies, reply: this.chain.replies[i] }]);
+		}
+	};
+
+	searchRecursive(arry) {
+		let obj = arry[arry.length - 1];
+		if (this.isMatchSearch(obj.reply)) {
+			this.results.push(arry);
+		}
+
+		for (let i = 0; i < obj.reply.next.replies.length; i++) {
+			var ary = [...arry];
+			ary.push({ replies: obj.reply.next.replies, reply: obj.reply.next.replies[i] });
+			this.searchRecursive(ary);
+		}
+	};
+
+	isMatchSearch(reply) {
+		return this.isTextMatchSearch(reply.id)
+			|| this.isTextMatchSearch(reply.text)
+			|| this.isTextMatchSearch(reply.next.text);
+	};
+
+	isTextMatchSearch(text) {
+		return text.toLowerCase().includes(this.searchText);
+	}
+}
