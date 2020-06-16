113c7cef5b33aa328a47f5eee34f8dff3dee9b7e
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2020-06-16 12:46:49 +0300

    support sass in style

--- src/components/automated/ReplyNode.vue
+++ src/components/automated/ReplyNode.vue
@@ -1,15 +1,6 @@
 <template>
     <div :class="['reply-container',selected||hover ?'reply-active':'']" @mouseover="hover = true" @mouseleave="hover = false">
-        <input type="text" 
-        v-model="text" 
-        :placeholder="placeHolder" 
-        :style="{'width':width}"
-        @input="handelChange" 
-        @blur="handelFocus(false)" 
-        @focus="handelFocus(true)" 
-        @keypress.enter="handelEnter"
-        >
-
+        <input type="text" v-model="text" :placeholder="placeHolder" :style="{'width':width}" @input="handelChange" @blur="handelFocus(false)" @focus="handelFocus(true)" @keypress.enter="handelEnter">
         <transition name="fade">
             <div v-show="hover">
                 <span @click="handelDelete">X</span>
@@ -56,7 +47,7 @@
             handelDelete() {
                 this.$emit('delete');
             },
-            handelEnter(){
+            handelEnter() {
                 this.$emit('enterPressed');
             }
         },
@@ -67,51 +58,55 @@
         }
     }
 </script>
-<style>
+<style lang="scss">
+    $reply-color-b:#3A97F9;
+    $reply-color-f:white;
+    $reply-place-holder:#82BEFD;
+    
     .reply-container {
         display: inline-block;
         margin: 5px;
         padding: 5px 15px;
         border-radius: 25px;
-        border: 1px solid #3A97F9;
-        background-color: white;
+        border: 1px solid $reply-color-b;
+        background-color: $reply-color-f;
         position: relative;
-    }
-    .reply-container input {
-        min-width: 12px;
-        max-width: 200px;
-        height: 30px;
-        background-color: transparent;
-        border: 0px;
-        color: #3A97F9;
-    }
-    .reply-container input::placeholder {
-        color: #82BEFD;
-    }
-    .reply-container div {
-        display: inline-block;
-        width: 21px;
-        height: 19px;
-        position: absolute;
-        top: -4px;
-        right: -8px;
-        background-color: white;
-        color: #3A97F9;
-        border-radius: 14px;
-        border: 1px solid #3A97F9;
-        cursor: pointer;
-        text-align: center;
-        padding-top: 2px;
-    }
-    .reply-container div:hover {
-        background-color: #3A97F9;
-        color: white;
-        border-color: white;
+        input {
+            min-width: 12px;
+            max-width: 200px;
+            height: 30px;
+            background-color: transparent;
+            border: 0px;
+            color: $reply-color-b;
+        }
+        input::placeholder {
+            color: $reply-place-holder;
+        }
+        div {
+            display: inline-block;
+            width: 21px;
+            height: 19px;
+            position: absolute;
+            top: -4px;
+            right: -8px;
+            background-color: $reply-color-f;
+            color: $reply-color-b;
+            border-radius: 14px;
+            border: 1px solid $reply-color-b;
+            cursor: pointer;
+            text-align: center;
+            padding-top: 2px;
+        }
+        div:hover {
+            background-color: $reply-color-b;
+            color: $reply-color-f;
+            border-color: $reply-color-f;
+        }
     }
     .reply-active {
-        background-color: #3A97F9;
-    }
-    .reply-active input {
-        color: white;
+        background-color: $reply-color-b;
+        input {
+            color: $reply-color-f;
+        }
     }
 </style>
