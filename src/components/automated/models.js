452d70374c7e67b54c810f303fb573fb0db8af32
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2020-06-18 00:43:44 +0300

    enhance search service

--- src/components/automated/models.js
+++ src/components/automated/models.js
@@ -29,47 +29,86 @@ export class Dialog {
 export var tree = new Chain();
 
 
+function unselectChainReplies(chain) {
+	if (!chain)
+		return;
+	for (let i = 0; i < chain.replies.length; i++) {
+		let reply = chain.replies[i];
+		reply.selected = false;
+		unselectChainReplies(reply.next);
+	}
+}
+
+export var ApplySearchResult = function (chain, result) {
+	// unselect all reply nodes
+	unselectChainReplies(chain);
+	// select replies
+	for (let i = 0; i < result.length; i++) {
+		let reply = result[i];
+		reply.selected = true;
+	}
+}
+
 export class SearchService {
-	constructor(chain, searchText) {
+	constructor(chain, searchText, callback = null) {
 		this.searchText = searchText;
 		this.chain = chain;
 		this.results = [];
-		this.search();
+		this.search(callback);
 	};
 
-	search() {
+	async search(callback = null) {
 		this.results = [];
 		if (!this.chain || !this.searchText)
 			return;
 
-		if (this.isTextMatchSearch(this.chain.id) || this.isTextMatchSearch(this.chain.text))
+		if (this.isChainMatchSearch(this.chain))
 			this.results.push([]);
 
 		for (let i = 0; i < this.chain.replies.length; i++) {
-			this.searchRecursive([{ replies: this.chain.replies, reply: this.chain.replies[i] }]);
+			this.searchRecursive([this.chain.replies[i]]);
 		}
+
+		if (callback)
+			callback(this);
 	};
 
 	searchRecursive(arry) {
-		let obj = arry[arry.length - 1];
-		if (this.isMatchSearch(obj.reply)) {
+		let reply = arry[arry.length - 1];
+		if (this.isReplyMatchSearch(reply)) {
 			this.results.push(arry);
 		}
 
-		for (let i = 0; i < obj.reply.next.replies.length; i++) {
-			var ary = [...arry];
-			ary.push({ replies: obj.reply.next.replies, reply: obj.reply.next.replies[i] });
-			this.searchRecursive(ary);
+		for (let i = 0; i < reply.next.replies.length; i++) {
+			var a = [...arry];
+			a.push(reply.next.replies[i]);
+			this.searchRecursive(a);
 		}
 	};
 
-	isMatchSearch(reply) {
+	isReplyMatchSearch(reply) {
 		return this.isTextMatchSearch(reply.id)
 			|| this.isTextMatchSearch(reply.text)
-			|| this.isTextMatchSearch(reply.next.text);
+			|| this.isChainMatchSearch(reply.next);
+	};
+
+	isChainMatchSearch(chain) {
+		if (this.isTextMatchSearch(chain.id) || this.isTextMatchSearch(chain.text))
+			return true;
+		for (let i = 0; i < chain.dialogs.length; i++) {
+			let dialog = chain.dialogs[i];
+			if (this.isDialogMatchSearch(dialog))
+				return true;
+		}
+		return false;
+	};
+
+	isDialogMatchSearch(dialog) {
+		return this.isTextMatchSearch(dialog.id)
+			|| this.isTextMatchSearch(dialog.text);
 	};
 
 	isTextMatchSearch(text) {
-		return text.toLowerCase().includes(this.searchText);
-	}
+		return text && text.toLowerCase().includes(this.searchText);
+	};
 }
