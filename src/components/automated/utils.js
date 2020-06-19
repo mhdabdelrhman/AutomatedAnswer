e8b2921f86f94a779f949af925cd63dffa7e2301
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2020-06-20 00:14:33 +0300

    (#16) fix save bug

--- src/components/automated/utils.js
+++ src/components/automated/utils.js
@@ -1,12 +1,23 @@
-export var generateId =  function () {
+export var generateId = function () {
     // Math.random should be unique because of its seeding algorithm.
     // Convert it to base 36 (numbers + letters), and grab the first 9 characters
     // after the decimal.
     return Math.random().toString(36).substr(2, 9);
 };
 
-export var deepClone = function(obj){
+export var deepClone = function (obj) {
     return JSON.parse(JSON.stringify(obj));
 };
 
+export var saveToLocalStorage = function (chainObject, storage) {
+    if (chainObject) {
+        localStorage.setItem(storage, JSON.stringify(chainObject));
+    }
+};
 
+export var loadFromLocalStorage = function (storage) {
+    if (localStorage.getItem(storage) && localStorage.getItem(storage).length > 0) {
+        return JSON.parse(localStorage.getItem(storage));
+    }
+    return null;
+};
