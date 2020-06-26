b798136a0d1a41a856ca92ee56c7f40d7cc7770c
MohammedAbdAlrehman <moh.3bd.rhman@gmail.com>
2020-06-27 02:22:39 +0300

    (#3) develop dialog UI

--- src/components/automated/DialogModal.vue
+++ src/components/automated/DialogModal.vue
@@ -1,37 +1,51 @@
 <template>
-  <div class="dialog-modal">
-    <v-btn @click="dialog = true">
-      Click
-    </v-btn>
+  <div>
     <v-dialog v-model="dialog" persistent scrollable max-width="500px">
-      <v-card class="pa-2">
+      <v-card class="pa-4">
         <v-menu>
           <template v-slot:activator="{ on }">
-                <v-btn color="primary"  v-on="on" class="mt-2">
-                  {{selectedOptionTitle}}
-                </v-btn>
+            <v-btn color="primary" v-on="on" class="mt-2">{{title}}</v-btn>
           </template>
           <v-list>
-            <v-list-item v-for="(option,index) in options" :key="index" @click="handelSelectOption(option,index)" dense>{{option.text}}
-            </v-list-item>
+            <v-list-item
+              v-for="(option,index) in options"
+              :key="index"
+              @click="handelSelectOption(option,index)"
+              dense
+            >{{option.text}}</v-list-item>
           </v-list>
         </v-menu>
-        <v-card-actions v-if="showMessageText">
-          <v-text-field prepend-icon="mdi-android-messages" label="Message"></v-text-field>
+        <v-card-actions v-if="isOptionSelected">
+          <v-text-field v-model="message" prepend-icon="mdi-android-messages" label="Message"></v-text-field>
         </v-card-actions>
-        <v-card-actions>          
-          <v-file-input 
-          v-if="selectedOption && selectedOption.type=='file'" 
-          :rules="rules" 
-          :accept="selectedOption.input.accept" 
-          :placeholder="selectedOption.placeholder"
-          :prepend-icon="selectedOption.icon" 
-          :label="selectedOption.text">
-          </v-file-input>
+        <v-card-actions v-if="inputMessage">
+          <span>{{inputMessage}}</span>
         </v-card-actions>
-        <v-card-actions>
-          <v-btn :rounded="true" @click="handelClose">Close</v-btn>
-          <v-btn class="ml-3" :rounded="true" :color="`success`" @click="handelSave" v-if="showSaveButton">Save</v-btn>
+        <v-card-actions v-if="fileInput">
+          <v-file-input
+            :rules="rules"
+            :accept="fileInput.accept"
+            :placeholder="fileInput.placeholder"
+            :prepend-icon="fileInput.icon"
+            :label="selectedOption.text"
+          ></v-file-input>
+        </v-card-actions>
+        <v-card-actions v-if="textInput">
+          <v-text-field
+            :placeholder="textInput.placeholder"
+            :prepend-icon="textInput.icon"
+            :label="selectedOption.text"
+          ></v-text-field>
+        </v-card-actions>
+        <v-card-actions class="mt-3 flex-row-reverse">
+          <v-btn
+            v-if="isOptionSelected"
+            class="ml-3"
+            :rounded="true"
+            :color="`success`"
+            @click="handelSave"
+          >Save</v-btn>
+          <v-btn :rounded="true" @click="handelClose">Cancel</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -39,50 +53,119 @@
 </template>
 
 <script>
-  import dialog_options from './config';
-  export default {
-    data() {
-      return {
-        options: dialog_options,
-        selectedOption: null,
-        htmlContent: "",
-        dialog: false,
-        rules: [
-          (value) => !value || value.size < 2000000 || "file size should be less than 2 MB!",
-        ],
+import options from "./config";
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: null
+    },
+    values: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    return {
+      options: options,
+      selectedOption: this.option,
+      message: "",
+      dialog: true,
+      rules: [
+        value =>
+          !value ||
+          value.size < 2000000 ||
+          "file size should be less than 2 MB!"
+      ]
+    };
+  },
+  computed: {
+    isOptionSelected() {
+      return this.selectedOption != null;
+    },
+    title() {
+      if (!this.isOptionSelected) return "Select dialog type";
+      return this.selectedOption.title || this.selectedOption.text;
+    },
+    fileInput() {
+      if (this.isOptionSelected && this.selectedOption.inputs) {
+        for (var i = 0; i < this.selectedOption.inputs.length; i++)
+          if (this.selectedOption.inputs[i].input_type == "file")
+            return this.selectedOption.inputs[i];
+      }
+      return null;
+    },
+    textInput() {
+      if (this.isOptionSelected && this.selectedOption.inputs) {
+        for (var i = 0; i < this.selectedOption.inputs.length; i++)
+          if (this.selectedOption.inputs[i].input_type == "text")
+            return this.selectedOption.inputs[i];
+      }
+      return null;
+    },
+    inputMessage() {
+      if (this.fileInput || this.textInput) {
+        if (
+          this.fileInput &&
+          this.fileInput.required &&
+          this.textInput &&
+          this.textInput.required
+        )
+          return "Please pick a file and set the url";
+
+        if (
+          this.fileInput &&
+          !this.fileInput.required &&
+          this.textInput &&
+          !this.textInput.required
+        )
+          return "Please pick a file or set the url";
+
+        if (this.fileInput && this.fileInput.required)
+          return "Please pick a file";
+
+        if (this.textInput && this.textInput.required)
+          return "Please set the url";
+      }
+      return null;
+    }
+  },
+  methods: {
+    handelSelectOption(option, index) {
+      this.selectedOption = option;
+    },
+    handelClose() {
+      this.dialog = false;
+      this.$emit("hide");
+    },
+    handelSave() {
+      if (this.isValidLocaly()) {
+        this.postToServer(data => {
+          if (data.success) {
+            this.dialog = false;
+            this.$emit("save", {
+              option: this.selectedOption,
+              values: { message: this.message }
+            });
+          } else {
+            this.showServerErrorMessage(data.error);
+          }
+        });
       }
     },
-    computed: {
-      showSaveButton() {
-        return this.selectedOption != null;
-      },
-      showMessageText() {
-        return this.selectedOption != null;
-      },
-      selectedOptionTitle() {
-        if (this.selectedOption == null)
-          return "Select dialog type";
-        return this.selectedOption.title || this.selectedOption.text;
-      },
+    isValidLocaly() {
+      return true;
     },
-    methods: {
-      handelSelectOption(option, index) {
-        this.selectedOption = option;
-        this.htmlContent = `<h2 @Click="handelClose">${this.selectedOptionTitle}</h2>`;
-      },
-      handelClose() {
-        this.dialog = false;
-      },
-      handelSave() {
-        this.dialog = false;
-      },
-    },
-  };
+    postToServer(callback) {
+      // post here to server
+      callback({ success: true, error: "" });
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-  .dialog-modal {}
-  .v-list-item {
-    min-height: 34px !important;
-  }
+.v-list-item {
+  min-height: 34px !important;
+}
 </style>
--- src/components/automated/config.js
+++ src/components/automated/config.js
@@ -1,136 +1,139 @@
 export default [
-    {
-        text: "Send photo",
+  {
+    id: "send-photo",
+    text: "Send photo",
+    inputs: [
+      {
         icon: "mdi-camera",
         placeholder: "Pick an avatar",
-        type: "file",
-        input: {
-            data_type: "photo",
-            accept: "image/png, image/jpeg, image/bmp",
-            required: "file-or-url", // file-or-url, file, url
-        }
-    },
-    {
-        text: "Send audio",
+        input_type: "file",
+        data_type: "photo",
+        accept: "image/png, image/jpeg, image/bmp",
+        required: true,
+      },
+      {
+        icon: "mdi-web",
+        placeholder: "Set avatar URL",
+        input_type: "text",
+        data_type: "url",
+        required: true,
+      },
+    ],
+  },
+  {
+    id: "send-audio",
+    text: "Send audio",
+    inputs: [
+      {
         icon: "mdi-phone-outline",
-        placeholder: "Pick an voice",
-        type: "file",
-        input: {
-            data_type: "audio",
-            accept: "MP3/mp3, M4A/m4a",
-            required: "file-or-url",
-        }
-    },
-    {
-        text: "Send video",
+        placeholder: "Pick a voice",
+        input_type: "file",
+        data_type: "audio",
+        accept: "MP3/mp3, M4A/m4a",
+        required: true,
+      },
+      {
+        icon: "mdi-web",
+        placeholder: "Set voice URL",
+        input_type: "text",
+        data_type: "url",
+        required: false,
+      },
+    ],
+  },
+  {
+    id: "send-video",
+    text: "Send video",
+    inputs: [
+      {
         icon: "mdi-video",
-        placeholder: "Pick an video",
-        type: "file",
-        input: {
-            data_type: "video",
-            accept: "video/mp4, video/mkv",
-            required: "file-or-url",
-        }
-    },
-    {
-        text: "Send document",
+        placeholder: "Pick a video",
+        input_type: "file",
+        data_type: "video",
+        accept: "video/mp4, video/mkv",
+        required: false,
+      },
+      {
+        icon: "mdi-web",
+        placeholder: "Set video URL",
+        input_type: "text",
+        data_type: "url",
+        required: false,
+      },
+    ],
+  },
+  {
+    id: "send-document",
+    text: "Send document",
+    inputs: [
+      {
         icon: "mdi-file-document",
-        placeholder: "Pick an document",
-        type: "file",
-        input: {
-            data_type: "document",
-            accept: "PSD/psd, PDF/pdf",
-            required: "file-or-url",
-        }
-    },
-    {
-        text: "Send RSS",
+        placeholder: "Pick a document",
+        input_type: "file",
+        data_type: "document",
+        accept: "PSD/psd, PDF/pdf",
+        required: true,
+      },
+      {
+        icon: "mdi-web",
+        placeholder: "Set document URL",
+        input_type: "text",
+        data_type: "url",
+        required: true,
+      },
+    ],
+  },
+  {
+    id: "send-rss",
+    text: "Send RSS",
+    inputs: [
+      {
         icon: "mdi-web",
         placeholder: "Set RSS URL",
-        type: "text",
-        input: {
-            data_type: "url",
-            required: "url", // url
-        }
-    },
-    {
-        text: "Ask for user email",
-        title: "Email",
-        icon: "mdi-email",
-        placeholder: "Email address",
-        type: "text",
-        input: {
-            data_type: "email"
-        }
-    },
-    {
-        text: "Ask for user phone number",
-        title: "Phone Number",
-        icon: "mdi-phone-outline",
-        placeholder: "Phone number",
-        type: "text",
-        input: {
-            data_type: "phone_number"
-        }
-    },
-    {
-        text: "Ask user to send a photo",
-        title: "Photo",
-        icon: "mdi-web",
-        placeholder: "Photo url",
-        type: "text",
-        input: {
-            data_type: "photo"
-        }
-    },
-    {
-        text: "Ask user to send an audio",
-        title: "Audio",
-        icon: "mdi-web",
-        placeholder: "Audio url",
-        type: "text",
-        input: {
-            data_type: "audio"
-        }
-    },
-    {
-        text: "Ask user to send a video",
-        title: "Video",
-        icon: "mdi-web",
-        placeholder: "Video url",
-        type: "text",
-        input: {
-            data_type: "video"
-        }
-    },
-    {
-        text: "Ask user to send a document",
-        title: "Document",
-        icon: "mdi-web",
-        placeholder: "Document url",
-        type: "text",
-        input: {
-            data_type: "document"
-        }
-    },
-    {
-        text: "Ask user to send contact CSV file",
-        title: "Contact",
-        icon: "mdi-web",
-        placeholder: "Contact url",
-        type: "text",
-        input: {
-            data_type: "contact"
-        }
-    },
-    {
-        text: "Ask user to send GPS location",
-        title: "GPS",
-        icon: "mdi-web",
-        placeholder: "GPS data",
-        type: "text",
-        input: {
-            data_type: "gps"
-        }
-    },
+        input_type: "text",
+        data_type: "url",
+        required: true,
+      },
+    ],
+  },
+  {
+    id: "ask-email",
+    text: "Ask for user email",
+    title: "Email",
+  },
+  {
+    id: "ask-phone",
+    text: "Ask for user phone number",
+    title: "Phone Number",
+  },
+  {
+    id: "ask-photo",
+    text: "Ask user to send a photo",
+    title: "Photo",
+  },
+  {
+    id: "ask-audio",
+    text: "Ask user to send an audio",
+    title: "Audio",
+  },
+  {
+    id: "ask-video",
+    text: "Ask user to send a video",
+    title: "Video",
+  },
+  {
+    id: "ask-document",
+    text: "Ask user to send a document",
+    title: "Document",
+  },
+  {
+    id: "ask-contact",
+    text: "Ask user to send contact CSV file",
+    title: "Contact",    
+  },
+  {
+    id: "ask-gps",
+    text: "Ask user to send GPS location",
+    title: "GPS",
+  },
 ];
